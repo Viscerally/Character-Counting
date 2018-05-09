a3084bc43002e08b645d1b2b4fb009a21eabc0f3
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-05-09 20:42:25 +0000

    made function that counts letter instances

---
+++ characters.js
@@ -0,0 +1,22 @@
+function countLetters(string) {
+    //Write a function that can return to us all the unique characters that exist in a string that is passed into the function
+    //f() should return instances of each letter in string
+    var newString = string.replace(/ /g, '').toLowerCase();//.replace takes out any spaces
+    var sorted = {};
+    for (var i = 0; i < newString.length; i++) {
+        var character = newString.charAt(i);
+        if (sorted[character]){
+            sorted[character]++;
+        }  else {
+            sorted[character] = 1;   
+        }  
+    
+
+    // console.log(newString[i]);
+    }
+  return sorted;
+}
+//this object will store our keys
+
+console.log(countLetters("Lighthouse in the house"));
+
